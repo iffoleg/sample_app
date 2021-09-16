aa38a63391ee00c88275a6f37584a6fd4513af07
iffoleg <55596087+iffoleg@users.noreply.github.com>
2021-09-16 17:02:33 +0300

    Finish log in/log out

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,34 +1,36 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
-  
+
   test "should be valid" do
     assert @user.valid?
   end
 
   test "name should be present" do
-    @user.name = "    "
+    @user.name = "     "
     assert_not @user.valid?
   end
-    
+
   test "email should be present" do
-    @user.email = "    "
+    @user.email = "     "
     assert_not @user.valid?
   end
-    
+
   test "name should not be too long" do
     @user.name = "a" * 51
     assert_not @user.valid?
-  end 
+  end
 
   test "email should not be too long" do
-    @user.email = "a" * 256
+    @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
-  end 
-    
+  end
+
   test "email validation should accept valid addresses" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                          first.last@foo.jp alice+bob@baz.cn]
@@ -47,11 +49,24 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
-  test "email addresses should be unique" do
+  test "email address should be unique" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
-    
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+  
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
